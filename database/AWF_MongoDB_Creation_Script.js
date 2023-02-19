ebf8ce9c99c0ef0bb60ef34db56b5020868c1bb0
Isaiah Andrews <59889246+SemiDoge@users.noreply.github.com>
2023-02-19 17:53:54 -0500

    Update AWF_MongoDB_Creation_Script.js

--- database/AWF_MongoDB_Creation_Script.js
+++ database/AWF_MongoDB_Creation_Script.js
@@ -2,7 +2,7 @@
 db = connect( 'mongodb://localhost/AWFdb' );
 
 //Inserting History questions into the database.
-db.quizes.insertMany([
+db.quizzes.insertMany([
     { category: "History", qTxt: "The modern-day city Istanbul last used to be called:", qAns: 4, a1Txt: "Byzantium", a2Txt: "Tsargrad", a3Txt: "Ankara", a4Txt: "Constantinople"},
     { category: "History", qTxt: "Germany was unified in the 1800's by which state:", qAns: 3, a1Txt: "Austria", a2Txt: "Hanover", a3Txt: "Prussia", a4Txt: "Russia"},
     { category: "History", qTxt: "Canada's oldest company is:", qAns: 4, a1Txt: "Tim Hortons", a2Txt: "Royal Bank of Canada (RBC)", a3Txt: "Irving Oil", a4Txt: "Hudson's Bay Company (HBC)"},
@@ -12,7 +12,7 @@ db.quizes.insertMany([
 ]);
 
 //Inserting Science 
-db.quizes.insertMany([
+db.quizzes.insertMany([
     { category: "Science", qTxt: "JWST stands for:", qAns: 2, a1Txt: "Jack Weber Science Technology", a2Txt: "James Webb Space Telescope", a3Txt: "Jehova's Witness Space Telescope", a4Txt: "James Weber Scholarship Technique"},
     { category: "Science", qTxt: "The closest star to the Sun is:", qAns: 2, a1Txt: "Rigil Kentaurus", a2Txt: "Proxima Centauri", a3Txt: "Toilman", a4Txt: "Barnard's Star"},
     { category: "Science", qTxt: "The chemical symbol for Gold is:", qAns: 4, a1Txt: "Ag", a2Txt: "Gl", a3Txt: "Fe", a4Txt: "Au"},
@@ -20,7 +20,7 @@ db.quizes.insertMany([
 ]);
 
 //Inserting Math 
-db.quizes.insertMany([
+db.quizzes.insertMany([
     { category: "Math", qTxt: "(1 + 1)*(2 - 3) = ?:", qAns: 3, a1Txt: "1", a2Txt: "2", a3Txt: "-2", a4Txt: "3"},
     { category: "Math", qTxt: "The derivative of the function 4*x^2 = ?:", qAns: 4, a1Txt: "2*x", a2Txt: "4*x", a3Txt: "6*x", a4Txt: "8*x"},
     { category: "Math", qTxt: "1 + 1 = ?", qAns: 1, a1txt: "2", a2Txt: "3", a3Txt: "4", a4Txt: "5"}, 
@@ -28,7 +28,7 @@ db.quizes.insertMany([
 ]);
 
 //Inserting Literature 
-db.quizes.insertMany([
+db.quizzes.insertMany([
     { category: "Literature", qTxt: "War and Peace was written by:", qAns: 2, a1Txt: "William Shakespeare", a2Txt: "Leo Tolstoy", a3Txt: "Fyodor Dostoyevsky", a4Txt: "Margaret Gilbert"},
     { category: "Literature", qTxt: "The Handmaiden's Tale was written by:", qAns: 3, a1Txt: "Jane Austen", a2Txt: "Mary Shelley", a3Txt: "Margaret Atwood", a4Txt: "Margaret Thatcher"},
     { category: "Literature", qTxt: "", qAns: 0, a1Txt: "", a2Txt: "", a3Txt: "", a4Txt: ""}/*STUB*/, 
