48b19c5de194d6c45161afb205ac9f6b520d9dc8
SemiDoge <taelforlife@gmail.com>
2023-02-21 05:01:23 -0500

    Start stubbing /leaderboard
    Just started to stub out the /leaderboard api endpoint.

--- backend/app.js
+++ backend/app.js
@@ -4,13 +4,19 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-//test - beyond dummy data
-var quizzes = [
+//test - beyond dummy data, meant to look like mongodb entries
+const quizzes = [
   {_id: "GuId832645", category: "Science", qTxt: "The chemical symbol for Gold is:", qAns: 4, a1Txt: "Ag", a2Txt: "Gl", a3Txt: "Fe", a4Txt: "Au"},
   {_id: "gUiD696969", category: "Literature", qTxt: "War and Peace was written by:", qAns: 2, a1Txt: "William Shakespeare", a2Txt: "Leo Tolstoy", a3Txt: "Fyodor Dostoyevsky", a4Txt: "Margaret Gilbert"},
   {_id: "GUID797979", category: "Literature", qTxt: "The Handmaiden's Tale was written by:", qAns: 3, a1Txt: "Jane Austen", a2Txt: "Mary Shelley", a3Txt: "Margaret Atwood", a4Txt: "Margaret Thatcher"}
 ];
-  
+
+const leaderboard = [
+  {_id: "ggguuiiddd", category: "History", userID: "GUID_1", finalScore: 60, timeStamp: "2023-02-21 13:00:00"},
+  {_id: "gGguuiidDd", category: "History", userID: "GUID_2", finalScore: 59, timeStamp: "2023-02-21 13:02:00"},
+];
+
+
 app.use(express.json());
 
 app.get("/styles.css", function(req, res) {
@@ -65,6 +71,18 @@ app.post("/quiz", (req, res) => {
 
   console.log(leaderboardEntry);
   res.status(200).send();
+});
+
+app.get("/leaderboard", (req, res) => {
+  if (isEmptyObject(req.query)) {
+    res.contentType = res.type("json");
+    res.send(quizzes); 
+  }
+
+  //TODO: Filter by category (via mongodb .find()?, or via API?)
+
+  //TODO: Match by userID (via mondodb)
+
 });
 
 function validateLeaderboardEntry(requestBody) {
