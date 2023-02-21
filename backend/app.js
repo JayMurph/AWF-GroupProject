45230cf9cf4912d2151c2f719d137531b7a040a9
SemiDoge <taelforlife@gmail.com>
2023-02-21 03:18:45 -0500

    Add category filtering
    Added the ability to filter the (for now) stub data by quiz category.

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,13 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
+//const jsdom = require('jsdom');
+//const {JSDOM} = jsdom;
+//const {window} = new JSDOM().window;
+//global.document = document;
+
+//const $ = jQuery = require('jquery')(window);
+
 //test - beyond dummy data
 var quizzes = [
   {_id: "GuId832645", category: "Science", qTxt: "The chemical symbol for Gold is:", qAns: 4, a1Txt: "Ag", a2Txt: "Gl", a3Txt: "Fe", a4Txt: "Au"},
@@ -23,6 +30,32 @@ app.get("/", (req, res) => {
 }).listen(port);
 
 app.get("/quiz", (req, res) => {
+  if (isEmptyObject(req.query)) {
+    res.contentType = res.type("json");
+    res.send(quizzes); 
+  }
+
+  let filtered = [];
+
+  const filters = req.query.category;
+  console.log("Filtering by: [" + req.query.category + "]");
+  
+  //filter by category
+  filtered = quizzes.filter((item) => {
+    if (item.category === filters) {
+      return true;
+    }
+
+    return false;
+  });
+  
+  console.log(filtered);
   res.contentType = res.type("json");
-  res.send(quizzes);
+  res.send(filtered);
 });
+
+
+
+function isEmptyObject(obj) {
+  return !Object.keys(obj).length;
+}
