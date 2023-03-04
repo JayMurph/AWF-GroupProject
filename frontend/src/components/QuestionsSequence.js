60dde19ae5e7bb49c88b88705f001efcac60d540
josh murphy <jaymurph92@gmail.com>
2023-03-03 19:44:30 -0500

    implement some timer logic for questions

--- frontend/src/components/QuestionsSequence.js
+++ frontend/src/components/QuestionsSequence.js
@@ -5,7 +5,7 @@ import {
   FlexColumnContainer,
   QuestionSequenceHeaderContainer,
 } from "../StyledElements";
-import Timer from 'react-compound-timer';
+import Timer from "react-compound-timer";
 
 const QUESTION_DURATION = 20000;
 
@@ -15,59 +15,89 @@ export default class QuestionsSequence extends React.Component {
 
     this.state = {
       currPgIdx: 0,
-      pgCount: props.triviaQuestions.length + 1,
+      pgCount: props.triviaQuestions.length + 2,
       currTime: 0.0,
       category: props.category,
       triviaQuestionsArr: props.triviaQuestions,
-      answerInfoArr:[]
+      answerInfoArr: [],
     };
+
+    this.timerRef = React.createRef();
   }
 
   onQuestionAnswered = (userAnswerInfo) => {
+    const timer = this.timerRef.current;
+    timer.stop();
+
+    const elapsedTime = timer.getTime();
+    timer.reset();
+
+    userAnswerInfo.timeTakenMs = QUESTION_DURATION - elapsedTime;
     this.setState((state) => {
       let answerInfoArr = [...state.answerInfoArr];
       answerInfoArr.push(userAnswerInfo);
-      let currPgIdx = (state.currPgIdx + 1) % state.pgCount;
-
-      return ({
-        currPgIdx: currPgIdx,
+      return {
         answerInfoArr: answerInfoArr
-      })}
-    );
+      };
+    });
+
+    this.goToNextPage();
+    timer.start();
   };
 
   onStartButtonClick = () => {
-    this.setState({ currPgIdx: 1 });
+    this.timerRef.current.start();
+    this.goToNextPage();
+  };
+
+  onTimeElapsed = () => {
+    this.goToNextPage();
+  };
+
+  goToNextPage = () => {
+    if (this.state.currPgIdx < this.state.pgCount) {
+      this.setState((state) => ({
+        currPgIdx: state.currPgIdx + 1,
+      }));
+    }
   };
 
   render() {
     let content;
     let questionCountText;
 
-    if (this.state.currPgIdx > 0)  {
-        content = 
+    if (this.state.currPgIdx > 0) {
+      content = (
         <QuestionForm
           key={this.state.currPgIdx - 1}
-          triviaQuestion={this.state.triviaQuestionsArr[this.state.currPgIdx - 1]}
+          triviaQuestion={
+            this.state.triviaQuestionsArr[this.state.currPgIdx - 1]
+          }
           onQuestionAnswered={this.onQuestionAnswered}
-        />;
-        questionCountText = <h2>Question {this.state.currPgIdx} / {this.state.triviaQuestionsArr.length}</h2>;
-    }
-    else {
-      content = 
+        />
+      );
+      questionCountText = (
+        <h2>
+          Question {this.state.currPgIdx} /{" "}
+          {this.state.triviaQuestionsArr.length}
+        </h2>
+      );
+    } else {
+      content = (
         <QuizStartForm
           category={this.state.category}
           onButtonClick={this.onStartButtonClick}
         />
-        questionCountText = <h2>Question 1 / {this.state.triviaQuestionsArr.length}</h2>;
+      );
+      questionCountText = (
+        <h2>Question 1 / {this.state.triviaQuestionsArr.length}</h2>
+      );
     }
 
     return (
       <FlexColumnContainer>
         <QuestionSequenceHeaderContainer>
-          <div style={{ justifySelf: "start" }}>
-            {questionCountText}
-          </div>
+          <div style={{ justifySelf: "start" }}>{questionCountText}</div>
           <div
             style={{
               justifySelf: "center",
@@ -77,14 +107,23 @@ export default class QuestionsSequence extends React.Component {
             }}
           >
             <Timer
+              ref={this.timerRef}
               initialTime={QUESTION_DURATION}
-              lastUnit='ms'
+              lastUnit="ms"
               direction="backward"
               timeToUpdate={100}
               startImmediately={false}
+              checkpoints={[
+                {
+                  time: 0,
+                  callback: this.onTimeElapsed,
+                },
+              ]}
             >
               <h1 style={{ marginRight: "22px" }}>
-                <Timer.Milliseconds formatValue={val => (val * 0.001).toFixed(1)}/>
+                <Timer.Milliseconds
+                  formatValue={(val) => (val * 0.001).toFixed(1)}
+                />
               </h1>
               <h2>secs</h2>
             </Timer>
