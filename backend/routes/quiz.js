ddf1b89c49111adb8d3dc50904af22c48d5c0bc4
SemiDoge <taelforlife@gmail.com>
2023-03-12 22:43:25 -0400

    Implement POST /quiz endpoint
    Follow the schema laid out in the API docs and it should work.

--- backend/routes/quiz.js
+++ backend/routes/quiz.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var mongoose = require("mongoose");
 var router = express.Router();
+var Joi = require('joi');
 
 const quizSchema = require('../schema/quiz');
 const quizModel = mongoose.model("quizzes", quizSchema);
 
+const leaderboardSchema = require('../schema/leaderboard');
+const leaderboardModel = mongoose.model("leaderboard", leaderboardSchema);
+
+
+//TODO: make a db query that allows this list to be loaded dynamically
 const quizCats = ["history", "math", "literature", "science"];
 
 router.get('/', async (req, res) => {
@@ -55,6 +61,43 @@ router.get('/', async (req, res) => {
 
     //console.log(req.query);
 });
+
+router.post('/', async (req, res) => {
+    console.log(`/quiz got POST`);
+    console.log(req.body);
+
+    //use joi to validate POST body 
+    const {error} = validateLeaderboardEntry(req.body);
+    if (error) {
+        res.status(400).send(error.message);
+        console.log(error.message);
+        return;
+    }
+
+    //once validated move on to the mongoose insertion
+    try {
+        leaderboardModel.create({
+            userId: req.body.userId,
+            finalScore: req.body.finalScore,
+            timeStamp: req.body.timeStamp
+        });
+    } catch(err) {
+        console.error(err);
+    }
+
+    res.sendStatus(200);
+});
+
+function validateLeaderboardEntry(requestBody) {
+  //TODO: Once the project has become more fleshed out, come back and update the Joi schema to fit better.
+  const schema = Joi.object({
+    userId: Joi.string().alphanum().required(),
+    finalScore: Joi.number().min(0).required(),
+    timeStamp: Joi.string().required()
+  });
+
+  return schema.validate(requestBody);
+}
 
 function doesCategoryExist(cat) {
     var index = quizCats.indexOf(cat.toLowerCase());
