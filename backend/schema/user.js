c63bc62f312a491f224a7490c59aece693399140
SemiDoge <taelforlife@gmail.com>
2023-03-12 20:45:14 -0400

    Reorganize mongoose schemas

--- backend/schema/user.js
+++ backend/schema/user.js
@@ -1,34 +1,16 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const { Schema } = require('mongoose');
+const ObjectId = Schema.Types.ObjectId;
 
 const userSchema = new Schema({
-    firstName:{
-        type: String,
-        required: true
-    },
-    lastName:{
-        type: String,
-        required: true
-    },
-    userName: {
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true
-    },
-    birthDate:{
-        type: Date
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    passwordConfirmation:{
-        type: String,
-        required: true
-    }
+    _id:                  { type: ObjectId, required: true },
+    firstName:            { type: String,   required: true },
+    lastName:             { type: String,   required: true },
+    userName:             { type: String,   required: true },
+    email:                { type: String,   required: true },
+    birthDate:            { type: Date,     required: true},
+    password:             { type: String,   required: true },
+    passwordConfirmation: { type: String,   required: true }
 });
 
 module.exports = userSchema;
