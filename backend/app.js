42b11ef1e89ad00df9bcd61b9d18a4312ad88f57
SemiDoge <taelforlife@gmail.com>
2023-03-13 03:42:43 -0400

    Updated API documentation.
    Also added missing app.js code.

--- backend/app.js
+++ backend/app.js
@@ -15,15 +15,18 @@ var indexRouter = require('./routes/index');
 var quizRouter = require('./routes/quiz');
 var signupRouter = require('./routes/signup');
 var profileRouter = require('./routes/profile');
+var leaderboardRouter = require('./routes/leaderboard');
 
 app.use('/', indexRouter);
 app.use('/quiz', quizRouter);
 app.use('/signup', signupRouter);
 app.use('/profile', profileRouter);
+app.use('/leaderboard', leaderboardRouter);
 
 mongoose.set('strictQuery', true);
 main().catch(err => console.log(err));
 async function main() {
+    //await mongoose.connect(`mongodb://127.0.0.1/AWFdb`);
     await mongoose.connect(`mongodb+srv://mDremo5093:iLnyHvHoAJjnivhF@cluster0.unkvw0a.mongodb.net/quizzes`);
 }
 
