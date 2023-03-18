d225ecc5a2808e4abe0bfae30defede27ce2e527
josh murphy <jaymurph92@gmail.com>
2023-03-18 16:33:55 -0400

    expect API on port 3000

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ import SignUp from './pages/signup';
 import Quiz from './pages/quiz';
 import { AppContentContainer } from './StyledElements';
 
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
+export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001";
 
 function App() {
 return (
