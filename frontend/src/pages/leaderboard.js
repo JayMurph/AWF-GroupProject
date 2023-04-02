ab9aaac11eb6e3456e7f8124949334dafaca3fc7
josh murphy <jaymurph92@gmail.com>
2023-04-02 02:34:51 -0400

    factor out header text in leaderboard

--- frontend/src/pages/leaderboard.js
+++ frontend/src/pages/leaderboard.js
@@ -8,12 +8,13 @@ export default class Leaderboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      root: null,
+      content: null,
+      headerText: "",
       currCategory: "",
       categories: [],
     };
 
-    this.state.root = (
+    this.state.content = (
       <CategorySelection
         categories={this.state.categories}
         onCategorySelection={this.onCategorySelection}
@@ -28,7 +29,7 @@ export default class Leaderboard extends React.Component {
   updateRootWithCategories = (categories) => {
     this.setState({
       categories: categories,
-      root: (
+      content: (
         <CategorySelection
           key={null}
           categories={categories}
@@ -61,22 +62,19 @@ export default class Leaderboard extends React.Component {
         try {
           res = await res.json();
         } catch (err) {
-            // No high scores
+          // No high scores
           this.setState({
-            root: (
-              <>
-                <PageHeader>
-                  No{" "}
-                  {category.substring(0, 1).toUpperCase() +
-                    category.substring(1)}{" "}
-                  High Scores
-                </PageHeader>
-                <CategorySelection
-                  key={null}
-                  categories={this.state.categories}
-                  onCategorySelection={this.onCategorySelection}
-                />
-              </>
+            headerText:
+              "No " +
+              category.substring(0, 1).toUpperCase() +
+              category.substring(1) +
+              " High Scores",
+            content: (
+              <CategorySelection
+                key={null}
+                categories={this.state.categories}
+                onCategorySelection={this.onCategorySelection}
+              />
             ),
           });
           return;
@@ -87,33 +85,29 @@ export default class Leaderboard extends React.Component {
           this.setState({ currCategory: category });
           this.setState({
             // display leaderboard
-            root: (
-              <>
-                <PageHeader>
-                  {category.substring(0, 1).toUpperCase() +
-                    category.substring(1)}
-                </PageHeader>
-                <LeaderboardList initialItems={res} LeaderboardEndpoint={leaderboardEndpoint}/>
-              </>
+            headerText:
+              category.substring(0, 1).toUpperCase() + category.substring(1),
+            content: (
+              <LeaderboardList
+                initialItems={res}
+                LeaderboardEndpoint={leaderboardEndpoint}
+              />
             ),
           });
         } else {
-            // NO HIGH SCORES
+          // NO HIGH SCORES
           this.setState({
-            root: (
-              <>
-                <PageHeader>
-                  No{" "}
-                  {category.substring(0, 1).toUpperCase() +
-                    category.substring(1)}{" "}
-                  High Scores
-                </PageHeader>
-                <CategorySelection
-                  key={null}
-                  categories={this.state.categories}
-                  onCategorySelection={this.onCategorySelection}
-                />
-              </>
+            headerText:
+              "No " +
+              category.substring(0, 1).toUpperCase() +
+              category.substring(1) +
+              " High Scores",
+            content: (
+              <CategorySelection
+                key={null}
+                categories={this.state.categories}
+                onCategorySelection={this.onCategorySelection}
+              />
             ),
           });
           return;
@@ -123,6 +117,11 @@ export default class Leaderboard extends React.Component {
   };
 
   render() {
-    return this.state.root;
+    return (
+      <>
+        <PageHeader>{this.state.headerText}</PageHeader>
+        <>{this.state.content}</>
+      </>
+    );
   }
 }
