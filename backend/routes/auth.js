813f1c153c6db3a11919f10a5f6d6ed4031d25db
SemiDoge <taelforlife@gmail.com>
2023-04-07 21:12:17 -0400

    Implement and connect login to auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,26 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
-const { generateAccessToken, authToken, insertRefreshToken, removeRefreshToken, findRefreshToken } = require('../util/auth');
 
+const userModel = require('../models/user');
+const { generateAccessToken, authToken, insertRefreshToken, removeRefreshToken, findRefreshToken } = require('../util/auth');
+const { validateUserLogin } = require('../util/validation');
+
+//test endpoint used in test suite
 router.get('/data', authToken, (req, res, next) => {
     //console.log(req.tokenPayload);
     res.json({data: "This is the data that required authentication."});
 });
 
 router.post('/login', async (req, res, next) => {
-    //auth
 
-    const payload = {
-        sub: req.body.sub,
-        userName: req.body.userName,
+    const login = await tryLogin(req.body);
+    if(login.success === false) {
+        res.sendStatus(404);
+        return;    
     }
 
-    const accessToken = generateAccessToken(payload, "60s");
+    //TODO: After consultation with Frontend team, alter what gets encoded into the JWT 
+    const payload = {
+        sub: login._id,
+        userName: login.userName,
+        password: login.password
+    };
+
+    const accessToken = generateAccessToken(payload, process.env.ACCESS_TOKEN_LIFETIME);
     const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET);
     const result = await insertRefreshToken({refreshToken: refreshToken});
 
     return (result === true) ? res.json({accessToken: accessToken, refreshToken: refreshToken}) : res.sendStatus(500);
+    //TODO: Explore saving JWT tokens to Cookie storage.
 });
 
 router.delete('/logout', async (req, res) => {
@@ -30,16 +42,47 @@ router.delete('/logout', async (req, res) => {
     return (result === true) ? res.sendStatus(204) : res.sendStatus(500);
 });
 
-router.post('/token', async (req, res) => {
+router.post('/renew', async (req, res) => {
     const refreshToken = req.body.refreshToken;
     if (refreshToken == null) return res.sendStatus(401);
     if (!await findRefreshToken({refreshToken: refreshToken})) return res.sendStatus(403);
 
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);
-        const accessToken = generateAccessToken({sub: user.sub, userName: user.userName}, "60s");
+        const accessToken = generateAccessToken({sub: user.sub, userName: user.userName}, process.env.ACCESS_TOKEN_LIFETIME);
         res.json({accessToken: accessToken});
     });
 });
+
+async function tryLogin(requestBody) {
+    const {error} = validateUserLogin(requestBody);
+    if (error) {
+        return { success: "false" };
+    }
+
+    const user = await(userModel.findOne({userName: requestBody.userName}));
+    if (!user) {
+        console.log("Couldn't find user")
+        return { success: "false" };
+    }
+
+    if (user.password !== requestBody.password) {
+        console.log(`${user.password} vs ${requestBody.password}`);
+        console.log("Passwords do not match")
+        return { success: "false" };
+    }
+
+    //console.log("Login user lookup successful");
+    return {
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        userName: user.userName,
+        email: user.email,
+        birthDate: user.birthDate,
+        password: user.password,
+        success: 'true'
+    };
+}
 
 module.exports = router;
