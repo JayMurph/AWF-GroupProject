2912e6871105c4016c083312a5421d616b8f9b74
josh murphy <jaymurph92@gmail.com>
2023-04-16 03:30:14 -0400

    towards refreshing access token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -8,56 +8,75 @@ import About from "./pages/about";
 import Login from "./pages/login";
 import SignUp from "./pages/signup";
 import Quiz from "./pages/quiz";
-import { USER_ID_KEY, USERNAME_KEY, ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY, COOKIE_KEYS, PASSWORD_KEY, FIRST_NAME_KEY, EMAIL_KEY} from "./Storage";
+import {
+  USER_ID_KEY,
+  USERNAME_KEY,
+  ACCESS_TOKEN_KEY,
+  REFRESH_TOKEN_KEY,
+  COOKIE_KEYS,
+  PASSWORD_KEY,
+  FIRST_NAME_KEY,
+  EMAIL_KEY,
+} from "./Storage";
 import { AppContentContainer } from "./StyledElements";
 import Leaderboard from "./pages/leaderboard";
-import { decodeToken, isExpired } from 'react-jwt';
+import { decodeToken, isExpired } from "react-jwt";
 import { LogoutUser } from "./ApiCalls";
 
 export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
 
 function isAuthenticated(cookies) {
-  return cookies.accessToken && 
-    !isExpired(cookies.accessToken);
+  return cookies.accessToken && !isExpired(cookies.accessToken);
 }
 
 function App() {
   const [cookies, setCookie, removeCookie] = useCookies(COOKIE_KEYS);
-  const [authenticated, setAuthenticated] = useState(()=>isAuthenticated(cookies));
+  const [authenticated, setAuthenticated] = useState(() =>
+    isAuthenticated(cookies)
+  );
   const [userId, setUserId] = useState(cookies.userId);
 
-  console.log(cookies);
-  console.log(cookies.accessToken);
-  console.log(decodeToken(cookies.accessToken));
-  console.log(isExpired(cookies.accessToken));
+  const onLogin = async (tokens) => {
+    if (tokens) {
+      const dt = decodeToken(tokens.accessToken);
+      setCookies(
+        tokens.accessToken,
+        tokens.refreshToken,
+        dt.sub,
+        dt.userName,
+        dt.password,
+        dt.firstName,
+        dt.email
+      );
+      setUserId(dt.sub);
+      setAuthenticated(true);
+    }
+  };
 
   const onLogout = async () => {
     try {
       if (cookies.refreshToken) {
         await LogoutUser(cookies.refreshToken);
       }
-    }
-    catch (err) {
+    } catch (err) {
       console.log(err);
-    }
-    finally {
+    } finally {
       setAuthenticated(false);
       setUserId(null);
       clearCookies();
     }
   };
 
-  const onLogin = async (tokens) => {
-    if (tokens) {
-      const dt = decodeToken(tokens.accessToken);
-      setCookies(tokens.accessToken, tokens.refreshToken, dt.sub, dt.userName, dt.password, dt.firstName, dt.email);
-      setUserId(dt.sub);
-      setAuthenticated(true);
-    }
-  }
-
-  const setCookies = (accessToken, refreshToken, userId, username, password, firstName, email) => {
-    const options = {path:"/"};
+  const setCookies = (
+    accessToken,
+    refreshToken,
+    userId,
+    username,
+    password,
+    firstName,
+    email
+  ) => {
+    const options = { path: "/" };
     setCookie(ACCESS_TOKEN_KEY, accessToken, options);
     setCookie(REFRESH_TOKEN_KEY, refreshToken, options);
     setCookie(USER_ID_KEY, userId, options);
@@ -65,10 +84,10 @@ function App() {
     setCookie(PASSWORD_KEY, password, options);
     setCookie(FIRST_NAME_KEY, firstName, options);
     setCookie(EMAIL_KEY, email, options);
-  } 
+  };
 
-  const clearCookies = () => {
-    const options = {path:"/"};
+  const clearCookies = useCallback(() => {
+    const options = { path: "/" };
     removeCookie(ACCESS_TOKEN_KEY, options);
     removeCookie(REFRESH_TOKEN_KEY, options);
     removeCookie(USER_ID_KEY, options);
@@ -76,7 +95,23 @@ function App() {
     removeCookie(PASSWORD_KEY, options);
     removeCookie(FIRST_NAME_KEY, options);
     removeCookie(EMAIL_KEY, options);
-  }
+  }, [removeCookie]);
+
+  useEffect(() => {
+    if (authenticated) {
+      // set callback to refresh token
+      let authExpirySecs = decodeToken(cookies.accessToken).exp;
+      console.log(authExpirySecs);
+      let millisToExpiry = authExpirySecs * 1000 - Date.now();
+      const timer = setTimeout(() => {
+        // refresh token
+      }, millisToExpiry);
+      return () => clearTimeout(timer);
+    } else {
+      clearCookies();
+      return () => {};
+    }
+  }, [clearCookies, cookies.accessToken, authenticated]);
 
   return (
     <Router>
@@ -87,14 +122,16 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
           {authenticated ? (
-            <Route path="/quiz" element={<Quiz userId={userId} accessToken={cookies.accessToken}/>} />
+            <Route
+              path="/quiz"
+              element={
+                <Quiz userId={userId} accessToken={cookies.accessToken} />
+              }
+            />
           ) : (
             <>
-              <Route path="/login" element={<Login onLogin={onLogin}/>} />
-              <Route
-                path="/sign-up"
-                element={<SignUp />}
-              />
+              <Route path="/login" element={<Login onLogin={onLogin} />} />
+              <Route path="/sign-up" element={<SignUp />} />
             </>
           )}
         </Routes>
