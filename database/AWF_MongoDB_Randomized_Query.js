b5be51768ad3945d9f9962e71d2336960638fd2e
Isaiah Andrews <59889246+SemiDoge@users.noreply.github.com>
2023-04-26 00:54:50 -0400

    Delete AWF_MongoDB_Randomized_Query.js

--- database/AWF_MongoDB_Randomized_Query.js
+++
@@ -1,4 +0,0 @@
-//This script is just an example of how to do a randomized query.
-
-//Will return a single random quiz with in the History topic.
-db.quizes.aggregate([{$match: {category: "History"}}, {$sample: {size: 1} }])
